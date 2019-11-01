4210c557a06208e8bfff70900c25384f77ad8e66
Casey <caseyaduncan08@gmail.com>
2019-11-01 13:03:43 -0600

    Fixed to recognize all multi-contour characters (i,j,=)

--- Box_Character.py
+++ Box_Character.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import urllib
 
 # Import Photo
-#path = r'Z:\caseyduncan\Casey Duncan\CSM Grad School Work\2019\Fall\CSCI 575B - Machine Learning\ML Project\Data\Data - Equations\eqn_test.jpg'
-path = r'Z:\caseyduncan\Casey Duncan\CSM Grad School Work\2019\Fall\CSCI 575B - Machine Learning\ML Project\Data\Data - Equations\eqn_test2.jpg'
+path = r'Z:\caseyduncan\Casey Duncan\CSM Grad School Work\2019\Fall\CSCI 575B - Machine Learning\ML Project\Data\Data - Equations\eqn_test5.jpg'
 img = cv2.imread(path)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # Convert to gray
 
@@ -30,17 +29,10 @@ for cnt in contours:
 	max_y = right_bc[1]
 	chars_bb.append([min_x,min_y,max_x,max_y])
 
-for i in chars_bb:
-	print(i)
-
-# Delete near duplicate contours 
-# (for example: sometimes the dot on an "i" is thought of as two contours)
-
 # Find characters that are made of multiple contours (for example: "=" or "i")
-chars_bb_new = []
+chars_bb_new = chars_bb.copy()
 for i in range(len(chars_bb)-1):
 	cnt_i = chars_bb[i]
-	chars_bb_new.append(cnt_i)
 	i=0
 	for j in range(i+1,len(chars_bb)):
 		cnt_j = chars_bb[j]
@@ -58,21 +50,31 @@ for i in range(len(chars_bb)-1):
 			if i == 0:
 				chars_bb_new.remove(cnt_i)
 				i=i+1
-			# Delete near duplicate contours 
-			# (for example: sometimes the dot on an "i" is thought of as two contours)
-			j=0
-			for cnt_new in chars_bb_new:
-				cent_new = cnt_new[0]+(cnt_new[2] - cnt_new[0])/2
-				if cnt_j == cnt_new:
-					pass
-				elif (abs(cent_new - cent_j) <= 20) and (abs(cent_new - cent_j) > 0):
-					if j == 0:
-						chars_bb_new.remove(vals_new)
-						j=j+1
 
-# Delete duplicates
+# Delete near duplicate contours 
+# (for example: sometimes the dot on an "i" is thought of as two contours)
+chars_bb_new2 = chars_bb_new.copy()
+for i in range(len(chars_bb_new)-1):
+	cnt_i = chars_bb_new[i]
+	for j in range(i+1,len(chars_bb_new)):
+		cnt_j = chars_bb_new[j]
+		cent_i = cnt_i[0]+(cnt_i[2] - cnt_i[0])/2
+		cent_j = cnt_j[0]+(cnt_j[2] - cnt_j[0])/2
+		area_i = (cnt_i[2] - cnt_i[0])*(cnt_i[3] - cnt_i[1])
+		area_j = (cnt_j[2] - cnt_j[0])*(cnt_j[3] - cnt_j[1])
+		if cnt_j == cnt_i:
+			pass
+		elif (abs(cent_i - cent_j) <= 20):
+			if area_i > area_j:
+				if cnt_j in chars_bb_new2:
+					chars_bb_new2.remove(cnt_j)
+			elif area_i < area_j:
+				if cnt_i in chars_bb_new2:
+					chars_bb_new2.remove(cnt_i)
+
+# Delete duplicates contours
 chars_bb = []
-for i in chars_bb_new:
+for i in chars_bb_new2:
 	if i not in chars_bb:
 		chars_bb.append(i)
 
